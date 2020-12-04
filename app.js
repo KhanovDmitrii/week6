ce7ef4e30c4cefc0c76be45e6ae0f673049f6c8b
KhanovDmitrii <khanovds@mail.ru>
2020-12-04 22:30:09 +0300

    задание5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 export default function(express, bodyParser, createReadStream, crypto, http){
     const app = express();
-
+    const NAME = "khanovds";
 
     const CORS = {
         'Access-Control-Allow-Origin': '*',
@@ -13,13 +13,12 @@ export default function(express, bodyParser, createReadStream, crypto, http){
     .use((r, res, next) => { r.res.set(CORS); next(); })
     .use(bodyParser.urlencoded({ extended: true }))
     .get('/sha1/:input', r => {
-        const shasum = crypto.createHash('sha1');
-        shasum.update(r.params.input);
-    
-        r.res.send(shasum.digest('hex'));
+        let shasum = crypto.createHash('sha1');
+        shasum.update(req.params.input);
+        res.send(shasum.digest('hex'));
     })
     
-    .get('/login/', (req, res) => res.send('khanovds'))
+    .get('/login/', (req, res) => res.send(NAME))
     .get('/code/', (req, res) => {
         res.set({'Content-Type': 'text/plain; charset=utf-8'});
         createReadStream(import.meta.url.substring(7)).pipe(res);
@@ -27,16 +26,31 @@ export default function(express, bodyParser, createReadStream, crypto, http){
     ;
 
     app.all('/req/', (req, res) => {
-        const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
-
-        http.get(addr, (r, b = '') => {
-            r
-            .on('data', d => b += d)
-            .on('end', () => res.send(b));
-        });
+        let url = "";
+        if (req.method == "POST") {
+            url = await get_post(req);
+        } else {
+            url = req.url.replace(/^.*\?(.*)(&|$)/, "$1");
+        }
+        url = url.replace(/addr=/, "");
+        res.send(await r_q(url));
     })
-    .get('/hello/', (req, res) => res.send('khanovds'));
+    .all('*', (req, res) => res.send(NAME));
      
     return app;
+
+    function r_q(url) {
+        return new Promise(r => http.request(url, function(response) {
+            var str = '';
+
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function() {
+                r(str);
+            });
+        }).end())
+    }
 
 }
